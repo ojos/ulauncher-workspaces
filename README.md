d7dfdcafd7e84aebdb955e1b5dc7c306237033c9
Ido <ido@ojos.jp>
2021-01-15 10:40:14 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# ulauncher-workspaces
+
+> [Ulauncher](https://ulauncher.io) extension for launching Google Chrome using a chosen user profile.
+
+## Screenshot
+
+<!-- ![screenshot](screenshot.png) -->
+
+## Requirements
+
+- Ulauncher
+- Python 3
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```
+https://github.com/ojos/ulauncher-workspaces
+```
+
+## Contributing
+
+- Fork it!
+- Create your feature branch: git checkout -b feature/my-feature
+- Commit your changes: git commit -am 'Add some feature'
+- Push to the branch: git push origin my-new-feature
+- Submit a pull request :D
+
+## Authors
+
+- [Ido](https://github.com/ojos)
+- README.md copied from [Bruno Paz](https://github.com/brpaz)
+
+## License
+
+MIT &copy; [Ido
---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.shared.action.ExtensionCustomAction import \
+    ExtensionCustomAction
+from ulauncher.api.shared.action.RenderResultListAction import \
+    RenderResultListAction
+from ulauncher.api.shared.event import ItemEnterEvent, KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+
+
+def scan_workspaces(workspace_root_folder):
+    workspaces = [
+        ws for ws in os.listdir(workspace_root_folder) if os.path.isdir(os.path.join(workspace_root_folder, ws))
+    ]
+
+    return workspaces
+
+
+class DemoExtension(Extension):
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        workspace_root_folder = os.path.expanduser(extension.preferences['workspace_root_folder'])
+        workspaces = scan_workspaces(workspace_root_folder)
+
+        # Filter by query if inserted
+        query = event.get_argument()
+        if query:
+            query = query.strip().lower()
+            for ws in workspaces:
+                name = ws.lower()
+                if query not in name:
+                    workspaces.pop(ws)
+
+
+        # # Filter by query if inserted
+        # query = event.get_argument()
+        # if query:
+        #     query = query.strip().lower()
+        #     for folder in list(profiles.keys()):
+        #         name = profiles[folder]['name'].lower()
+        #         if query not in name:
+        #             profiles.pop(folder)
+
+        # Create launcher entries
+        entries = []
+        for ws in workspaces:
+            entries.append(ExtensionResultItem(
+                icon='images/icon.svg',
+                name=ws,
+                description=ws,
+                on_enter=ExtensionCustomAction({
+                    'open_cmd': extension.preferences['open_cmd'],
+                    'opt': [os.path.join(workspace_root_folder, ws)]
+                }, keep_app_open=True)
+            ))
+        return RenderResultListAction(entries)
+
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        # Open Chrome when user selects an entry
+        data = event.get_data()
+        cmd_path = data['open_cmd']
+        opt = data['opt']
+        subprocess.Popen([cmd_path] + opt)
+
+
+if __name__ == '__main__':
+    DemoExtension().run()
